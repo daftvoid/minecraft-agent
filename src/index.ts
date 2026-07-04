2d1dcfda81acf7727e008e45ef020d1c15e2cdda
afton <170838562+daftvoid@users.noreply.github.com>
2026-07-04 14:34:08 +0200

    feat: add ai

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import mineflayer from "mineflayer";
+import {OpenAI} from "openai";
 
 const bot = mineflayer.createBot({
     host: 'localhost',
@@ -7,9 +8,33 @@ const bot = mineflayer.createBot({
     // port: 25565,
 })
 
-bot.on('chat', (username, message) => {
+const client = new OpenAI({
+    baseURL: 'http://localhost:11434/v1',
+    apiKey: 'ignored'
+})
+
+bot.on('chat', async (username, message) => {
+    console.log(`<${username}> ${message}`);
+
     if (username === bot.username) return
-    bot.chat(message)
+
+    const res = await client.chat.completions.create({
+        model: "granite4:3b",
+        messages: [
+            { role: 'system', content:
+                    `You are a agent in the video game Minecraft.
+                     You are directly interacting with the player (in chat) and the minecraft world.
+                    
+                     Keep your answers really short, do not cross the message length limit of 255 characters.`
+            },
+            { role: 'system', content: `The name of the player you are talking to is "${username}"`},
+            { role: "user", content: message },
+        ],
+
+    })
+
+    const aimsg = res.choices[0]!.message.content
+    bot.chat(aimsg ?? '')
 })
 
 // Log errors and kick reasons:
